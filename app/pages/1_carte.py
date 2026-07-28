2cd98788b460f24b58d3eb186c2388334b038b76
Gale Knight <62402862+ThierryRossignol@users.noreply.github.com>
2026-07-28 23:00:30 +0200

    Update 1_carte.py

---
+++ app/pages/1_carte.py
@@ -0,0 +1,9 @@
+"""
+Page de cartographie.
+
+Fonctionnalités :
+- Affichage interactif de la carte de France.
+- Visualisation de l'Indice de Besoin de Mobilisation (IBM).
+- Application de filtres géographiques.
+- Affichage des informations principales d'un territoire.
+"""
