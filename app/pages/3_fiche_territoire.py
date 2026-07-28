c53c6a65f6a006663aab1cb822b0d303cac96c45
Gale Knight <62402862+ThierryRossignol@users.noreply.github.com>
2026-07-28 23:04:03 +0200

    Update 3_fiche_territoire.py

---
+++ app/pages/3_fiche_territoire.py
@@ -0,0 +1,9 @@
+"""
+Page de fiche territoire.
+
+Fonctionnalités :
+- Présentation détaillée d'une commune.
+- Affichage des composantes de l'IBM.
+- Visualisation des principaux indicateurs.
+- Historique des résultats électoraux.
+"""
