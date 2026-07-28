bc2c301d48c5ca660e3ac788908ab0ffd2d99af9
Gale Knight <62402862+ThierryRossignol@users.noreply.github.com>
2026-07-28 22:55:56 +0200

    Update mapping.py

---
+++ src/mapping.py
@@ -0,0 +1,13 @@
+"""
+Module de cartographie.
+
+Responsabilités :
+- Générer les cartes choroplèthes.
+- Associer les indicateurs aux limites administratives.
+- Définir les palettes de couleurs.
+- Gérer les couches cartographiques.
+- Préparer les données destinées au dashboard.
+
+Ce module est responsable uniquement de la représentation cartographique des
+résultats.
+"""
