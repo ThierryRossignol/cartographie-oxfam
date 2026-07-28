e4c4dee36780f50824b4bc176d7ae45165cae564
Gale Knight <62402862+ThierryRossignol@users.noreply.github.com>
2026-07-28 23:04:18 +0200

    Update 4_methodologie.py

---
+++ app/pages/4_methodologie.py
@@ -0,0 +1,11 @@
+"""
+Page de méthodologie.
+
+Contenu :
+- Présentation de l'IBM.
+- Description des variables.
+- Formules de calcul.
+- Sources des données.
+- Limites méthodologiques.
+- Documentation du projet.
+"""
