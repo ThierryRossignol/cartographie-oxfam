db3a1863372af6456a8b29583c5974d8adbc6ec6
Gale Knight <62402862+ThierryRossignol@users.noreply.github.com>
2026-07-28 23:03:29 +0200

    Update 2_classement.py

---
+++ app/pages/2_classement.py
@@ -0,0 +1,9 @@
+"""
+Page de classement des territoires.
+
+Fonctionnalités :
+- Classement des communes selon leur IBM.
+- Tri et filtrage des résultats.
+- Recherche d'une commune.
+- Comparaison entre plusieurs territoires.
+"""
